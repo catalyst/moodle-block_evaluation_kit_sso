1f1183fcebc9aff5ee740bbcb29785f3316a80ad
Dan Marsden <dan@danmarsden.com>
2018-08-21 15:27:05 +1200

    Improve perfomance of isUserStudent. This causes significant performance issues on sites where teachers are in a large number of courses and isstudent is evalutated for each course as false.
    Previous code used a config var to obtain which roles to check, but UI didn't allow that var to be set.
    and used fall-back of "student" role. Any upstream upgrades should be checked
    
    Previous function also used an inconsistant combination of my_..() functions and passing $user_id

--- Utils.php
+++ Utils.php
@@ -1,40 +1,20 @@
 <?php
 
-class EvalKitUtils
-{
+class EvalKitUtils {
 
-    function isUserStudent($user_id)
-    {
-	if (!empty($_SESSION['ekiamstudent']) && $_SESSION['ekiamstudent']=='TRUE')
-		return true;
+    function isUserStudent($user_id) {
+        global $DB;
+        if (!empty($_SESSION['ekiamstudent']) && $_SESSION['ekiamstudent'] == 'TRUE') {
+            return true;
+        }
 
-	$config = get_config('blocks/evaluation_kit_sso');
-      $studentRolesCsv = 'student'; //$config->EvalKitstudentroles;
-	if (empty($studentRolesCsv))
-		$studentRolesCsv = 'student';
-	$studentRoles = explode(',',$studentRolesCsv);
-
-	$courses = enrol_get_my_courses(NULL, 'visible DESC, fullname ASC');
-	foreach ($courses as $course) {
-		$course_id = $course->id;
-		$rolename  = 'student';
-
-		$myRoles = get_user_roles_in_course($user_id, $course_id);
-		$myRoles = explode(',',$myRoles);
-		foreach ($studentRoles as $sr) {
-			foreach ($myRoles as $mr) {
-				if (strlen(stristr($mr, $sr))>0) 
-				{
-					$_SESSION['ekiamstudent'] = 'TRUE';
-					return true;
-				}
-			}
-		}
-	}
-
-	$SESSION['ekiamstudent'] = 'FALSE';
-	return false;
+        $role = $DB->get_record('role', array('shortname' => 'student'));
+        if (user_has_role_assignment($user_id, $role->id)) {
+            $_SESSION['ekiamstudent'] = 'TRUE';
+            return true;
+        }
+        $SESSION['ekiamstudent'] = 'FALSE';
+        return false;
     }
-} 
-
+}
 ?>
