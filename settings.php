b163433dff4d5baa410e8f56e5dba0a01483bf7b
Dan Marsden <dan@danmarsden.com>
2018-08-21 15:24:09 +1200

    Settings performance - only include settings when admin->fulltree in use.

--- settings.php
+++ settings.php
@@ -1,16 +1,16 @@
 <?php
+if ($ADMIN->fulltree) {
+    $config = get_config('blocks/evaluation_kit_sso');
 
-$config = get_config('blocks/evaluation_kit_sso');
+    $configs = array();
+    $configs[] = new admin_setting_configtext('EvalKitaccounturl', 'EvaluationKIT Account URL', '', '', PARAM_TEXT);
+    $configs[] = new admin_setting_configtext('EvalKitconsumerkey', 'Consumer Key', '', '', PARAM_TEXT);
+    $configs[] = new admin_setting_configtext('EvalKitsharedsecretkey', 'Shared Secret Key', '', '', PARAM_TEXT);
 
-$configs = array();
-$configs[] = new admin_setting_configtext('EvalKitaccounturl', 'EvaluationKIT Account URL', '','', PARAM_TEXT);
-$configs[] = new admin_setting_configtext('EvalKitconsumerkey', 'Consumer Key', '','', PARAM_TEXT);
-$configs[] = new admin_setting_configtext('EvalKitsharedsecretkey', 'Shared Secret Key', '','', PARAM_TEXT);
-
-foreach ($configs as $config) {
-    $config->plugin = 'blocks/evaluation_kit_sso';
-    $settings->add($config);
+    foreach ($configs as $config) {
+        $config->plugin = 'blocks/evaluation_kit_sso';
+        $settings->add($config);
+    }
 }
-
 
 ?>
